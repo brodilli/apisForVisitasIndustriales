d15ad42a3c99eb037705647afb694cd303d98fdd
brodilli <brodilli_@hotmail.com>
2023-08-20 21:42:00 -0600

    se modificó la forma de obtener las visitas para traer los datos segun el semestre

--- obtener_solicitudes_visitas.php
+++ obtener_solicitudes_visitas.php
@@ -1,20 +1,46 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Content-Type: text/html; charset=utf-8");
-$method = $_SERVER['REQUEST_METHOD'];
-$sql= "SELECT solicitud_visita.id_visita, empresa.nombre_empresa, empresa.lugar, usuario.nombres, usuario.apellidoP, 
-usuario.apellidoM, usuario.id_usuario, solicitud_visita.fecha, solicitud_visita.horaSalida,solicitud_visita.horaLlegada, solicitud_visita.estatus, solicitud_visita.id_empresa, solicitud_visita.asignatura, solicitud_visita.objetivo, solicitud_visita.grupo, 
-solicitud_visita.semestre, solicitud_visita.num_alumnos, solicitud_visita.id_carrera, solicitud_visita.num_alumnas, solicitud_visita.comentarios,carrera.nombre_carrera FROM solicitud_visita 
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+$data = json_decode(file_get_contents("php://input"));
+$rango = $data -> rango;
+
+$mesActual = date('n'); // Obtiene el número del mes actual (1-12)
+$anoActual = date('Y'); // Obtiene el año actual
+if ($rango == "1"){
+    $sql = "SELECT solicitud_visita.id_visita, empresa.nombre_empresa, empresa.lugar, usuario.nombres, usuario.apellidoP, 
+usuario.apellidoM, usuario.id_usuario, solicitud_visita.fecha, solicitud_visita.horaSalida, solicitud_visita.horaLlegada, solicitud_visita.estatus, solicitud_visita.id_empresa, solicitud_visita.asignatura, solicitud_visita.objetivo, solicitud_visita.grupo, 
+solicitud_visita.semestre, solicitud_visita.num_alumnos, solicitud_visita.id_carrera, solicitud_visita.num_alumnas, solicitud_visita.comentarios, carrera.nombre_carrera 
+FROM solicitud_visita 
 INNER JOIN empresa on solicitud_visita.id_empresa = empresa.id_empresa 
 INNER JOIN usuario on solicitud_visita.id_usuario = usuario.id_usuario 
-INNER JOIN carrera on solicitud_visita.id_carrera = carrera.id_carrera;";
+INNER JOIN carrera on solicitud_visita.id_carrera = carrera.id_carrera";
+
+if ($rango === "1" && $mesActual >= 1 && $mesActual <= 6) {
+    // Agrega la condición para enero a junio del año actual
+    $sql .= " WHERE MONTH(solicitud_visita.fecha) BETWEEN 1 AND 6 AND YEAR(solicitud_visita.fecha) = $anoActual";
+}
+if ($rango === "1" && $mesActual >= 7 && $mesActual <= 12) {
+    // Agrega la condición para julio a diciembre del año actual
+    $sql .= " WHERE MONTH(solicitud_visita.fecha) BETWEEN 7 AND 12 AND YEAR(solicitud_visita.fecha) = $anoActual";
+}
+}else{
+    $sql= "SELECT solicitud_visita.id_visita, empresa.nombre_empresa, empresa.lugar, usuario.nombres, usuario.apellidoP, 
+    usuario.apellidoM, usuario.id_usuario, solicitud_visita.fecha, solicitud_visita.horaSalida,solicitud_visita.horaLlegada, solicitud_visita.estatus, solicitud_visita.id_empresa, solicitud_visita.asignatura, solicitud_visita.objetivo, solicitud_visita.grupo, 
+    solicitud_visita.semestre, solicitud_visita.num_alumnos, solicitud_visita.id_carrera, solicitud_visita.num_alumnas, solicitud_visita.comentarios,carrera.nombre_carrera FROM solicitud_visita 
+    INNER JOIN empresa on solicitud_visita.id_empresa = empresa.id_empresa 
+    INNER JOIN usuario on solicitud_visita.id_usuario = usuario.id_usuario 
+    INNER JOIN carrera on solicitud_visita.id_carrera = carrera.id_carrera;";
+}
+
 include "conectar.php";
 //sleep(1);
 function desconectar($conexion){
 
     $close = mysqli_close($conexion);
-
+        
         if($close){
             echo '';
         }else{
