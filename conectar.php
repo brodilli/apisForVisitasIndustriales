6c834c501a3099768fa1eb2c457960d05cfa7133
brodilli <brodilli_@hotmail.com>
2023-09-13 22:47:50 -0600

    se agregó las contraseñas para la base de datos

--- conectar.php
+++ conectar.php
@@ -5,19 +5,21 @@ header("Content-Type: text/html; charset=utf-8");
 $method = $_SERVER['REQUEST_METHOD'];
 
 function conectarDB(){
+
     $servidor = "localhost";
     $usuario = "agendaCitas";
     $password = "agendaCitas2023#";
     $bd = "visitas_industriales";
+  
 
-    try {
-        $conexion = new PDO("mysql:host=$servidor;dbname=$bd;charset=utf8", $usuario, $password);
-        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $conexion;
-    } catch (PDOException $e) {
-        echo 'Ha sucedido un error inesperado en la conexión de la base de datos: ' . $e->getMessage();
-        exit;
-    }
+    $conexion = mysqli_connect($servidor, $usuario, $password,$bd);
+
+        if($conexion){
+            
+        }else{
+            echo 'Ha sucedido un error inexperado en la conexion de la base de datos';
+        }
+
+    return $conexion;
 }
-
 ?>
