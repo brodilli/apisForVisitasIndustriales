c853523121a6f8f0d82525a8b8e8601baa1e76e2
brodilli <brodilli_@hotmail.com>
2023-09-17 23:05:22 -0600

    prueba api 7

--- login2.php
+++ login2.php
@@ -14,9 +14,9 @@ if ($data !== null && isset($data->correo) && isset($data->contraseña)) {
     $contraseña = $data->contraseña;
 
     try {
-        $stmt = $conn->prepare("SELECT * FROM `usuario` WHERE `correo` = :correo AND `contraseña` = :contraseña");
+        $stmt = $conn->prepare("SELECT * FROM `usuario` WHERE `correo` = :correo AND `contraseña` = :contrasena"); // Cambiado a :contrasena
         $stmt->bindParam(':correo', $correo);
-        $stmt->bindParam(':contraseña', $contraseña);
+        $stmt->bindParam(':contrasena', $contraseña); // Cambiado a :contrasena
 
         if ($stmt->execute()) {
             $rs = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -36,43 +36,7 @@ if ($data !== null && isset($data->correo) && isset($data->contraseña)) {
         http_response_code(500);
         echo json_encode(array("error" => $e->getMessage()));
     }
-} else {require 'conectar.php';
-    $conn = conectarDb();
-    
-    $data = json_decode(file_get_contents("php://input"));
-    
-    if ($data !== null && isset($data->correo) && isset($data->contraseña)) {
-        $correo = $data->correo;
-        $contraseña = $data->contraseña;
-    
-        try {
-            $stmt = $conn->prepare("SELECT * FROM `usuario` WHERE `correo` = :correo AND `contraseña` = :contrasena"); // Cambiado a :contrasena
-            $stmt->bindParam(':correo', $correo);
-            $stmt->bindParam(':contrasena', $contraseña); // Cambiado a :contrasena
-    
-            if ($stmt->execute()) {
-                $rs = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-                if ($rs) {
-                    http_response_code(200);
-                    echo json_encode(array("data" => $rs));
-                } else {
-                    http_response_code(404);
-                    echo json_encode(array("error" => "Usuario no encontrado"));
-                }
-            } else {
-                http_response_code(500);
-                echo json_encode(array("error" => "Error en la consulta SQL"));
-            }
-        } catch (PDOException $e) {
-            http_response_code(500);
-            echo json_encode(array("error" => $e->getMessage()));
-        }
-    } else {
-        http_response_code(400);
-        echo json_encode(array("error" => "Los datos de inicio de sesión son incorrectos."));
-    }
-    
+} else {
     http_response_code(400);
     echo json_encode(array("error" => "Los datos de inicio de sesión son incorrectos."));
 }
