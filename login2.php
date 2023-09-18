f7fee3bed5bbd8b968f893542df9d1e0d0047ba4
brodilli <brodilli_@hotmail.com>
2023-09-17 22:59:12 -0600

    prueba api 5

--- login2.php
+++ login2.php
@@ -1,9 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: access");
-header("Access-Control-Allow-Methods: POST");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Access-Control-Allow-Methods: POST');
+header('Content-Type: application/json; charset=UTF-8');
 
 require 'conectar.php';
 $conn = conectarDb();
@@ -13,39 +12,32 @@ $data = json_decode(file_get_contents("php://input"));
 if ($data !== null && isset($data->correo) && isset($data->contraseña)) {
     $correo = $data->correo;
     $contraseña = $data->contraseña;
-    // Resto del código
-} else {
-    // Manejar el caso en el que los datos no están presentes o son incorrectos
-    http_response_code(400); // Bad Request
-    echo json_encode(array("error" => "Los datos de inicio de sesión son incorrectos."));
-}
 
+    try {
+        $stmt = $conn->prepare("SELECT * FROM `usuario` WHERE `correo` = :correo AND `contraseña` = :contraseña");
+        $stmt->bindParam(':correo', $correo);
+        $stmt->bindParam(':contraseña', $contraseña);
 
-try {
-    $stmt = $conn->prepare("SELECT * FROM `usuario` WHERE `correo` = :correo AND `contraseña` = :contraseña");
-    $stmt->bindParam(':correo', $correo);
-    $stmt->bindParam(':contraseña', $contraseña);
+        if ($stmt->execute()) {
+            $rs = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($stmt->execute()) {
-        $rs = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if ($rs) {
-            http_response_code(200);
-            $outp = json_encode($rs);
-            echo $outp;
+            if ($rs) {
+                http_response_code(200);
+                echo json_encode(array("data" => $rs));
+            } else {
+                http_response_code(404);
+                echo json_encode(array("error" => "Usuario no encontrado"));
+            }
         } else {
-            http_response_code(404);
-            $outp = '{"error":"Usuario no encontrado"}';
-            echo $outp;
+            http_response_code(500);
+            echo json_encode(array("error" => "Error en la consulta SQL"));
         }
-    } else {
+    } catch (PDOException $e) {
         http_response_code(500);
-        $outp = '{"error":"Error en la consulta SQL"}';
-        echo $outp;
+        echo json_encode(array("error" => $e->getMessage()));
     }
-} catch (PDOException $e) {
-    http_response_code(500);
-    $outp = '{"error":"' . $e->getMessage() . '"}';
-    echo $outp;
+} else {
+    http_response_code(400);
+    echo json_encode(array("error" => "Los datos de inicio de sesión son incorrectos."));
 }
 ?>
