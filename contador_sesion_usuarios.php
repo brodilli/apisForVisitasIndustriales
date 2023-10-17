73bb7126e6e2f5cbeea9707e3365a6da47c5fdb7
brodilli <brodilli_@hotmail.com>
2023-10-16 23:25:32 -0600

    Se moficó la api de contador sesion usuario antiguo

--- contador_sesion_usuarios.php
+++ contador_sesion_usuarios.php
@@ -5,29 +5,28 @@ header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+
 require 'conectar.php';
+$conexion=conectarDb();
+$dataObject = json_decode(file_get_contents("php://input"));
 
-try {
-    $pdo = conectarDb(); // Reemplaza esta línea con la función que crea la conexión PDO
+$id_usuario = $dataObject-> id_usuario;
+$numSesion = $dataObject-> numSesion;
 
-    $dataObject = json_decode(file_get_contents("php://input"));
+if ($_SERVER['REQUEST_METHOD'] == 'POST')  {
+  $actualizacion = "UPDATE `usuario` SET 
+		`numSesion`='$numSesion'
+		WHERE id_usuario = $id_usuario";
+   
+   $resultadoActualizacion = mysqli_query($conexion, $actualizacion); 
 
-    $id_usuario = $dataObject->id_usuario;
-    $numSesion = $dataObject->numSesion;
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $stmt = $pdo->prepare("UPDATE `usuario` SET `numSesion` = :numSesion WHERE `id_usuario` = :id_usuario");
-        $stmt->bindParam(':numSesion', $numSesion, PDO::PARAM_INT);
-        $stmt->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
-        $stmt->execute();
-
-        if ($stmt->rowCount() > 0) {
-            echo json_encode(array('isOk' => true, 'msj' => 'Registro editado de forma exitosa.'));
-        } else {
-            echo json_encode(array('isOk' => false, 'msj' => 'No se encontraron registros para actualizar.'));
-        }
-    }
-} catch (PDOException $e) {
-    echo json_encode(array('isOk' => false, 'msj' => $e->getMessage()));
+   if($resultadoActualizacion)
+   {
+    echo json_encode(array('isOk'=>true,'msj'=>'Registro editado de forma exitosa.'));
+   }
+   else
+   {
+    echo json_encode(array('isOk'=>false,'msj'=>$conexion->error)); 
+   }
 }
 ?>
