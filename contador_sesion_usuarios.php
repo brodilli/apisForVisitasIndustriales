698b7479804390c48ae299e8d38651630ff9eeb6
brodilli <brodilli_@hotmail.com>
2023-10-16 23:14:15 -0600

    Se moficó la api de contador sesion usuario

--- contador_sesion_usuarios.php
+++ contador_sesion_usuarios.php
@@ -1,42 +1,35 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
-header('Content-Type: application/json; charset=UTF-8');
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 require 'conectar.php';
 
-$conexion = conectarDb();
-$dataObject = json_decode(file_get_contents("php://input"));
+try {
+    $pdo = conectarDb(); // Reemplaza esta línea con la función que crea la conexión PDO
 
-$id_usuario = isset($dataObject->id_usuario) ? $dataObject->id_usuario : null;
-$numSesion = isset($dataObject->numSesion) ? $dataObject->numSesion : null;
+    $dataObject = json_decode(file_get_contents("php://input"));
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($id_usuario !== null && $numSesion !== null) {
-        try {
-            $actualizacion = "UPDATE `usuario` SET `numSesion` = :numSesion WHERE `id_usuario` = :id_usuario";
-            
-            $stmt = $conexion->prepare($actualizacion);
-            $stmt->bindParam(':numSesion', $numSesion, PDO::PARAM_INT);
-            $stmt->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
-            
-            if ($stmt->execute()) {
-                http_response_code(200);
-                echo json_encode(array('isOk' => true, 'msj' => 'Registro editado de forma exitosa.'));
-            } else {
-                http_response_code(500);
-                echo json_encode(array('isOk' => false, 'msj' => 'Error al editar el registro.'));
-            }
-        } catch (PDOException $e) {
-            http_response_code(500);
-            echo json_encode(array('isOk' => false, 'msj' => 'Error en la base de datos: ' . $e->getMessage()));
+    $id_usuario = $dataObject->id_usuario;
+    $numSesion = $dataObject->numSesion;
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $stmt = $pdo->prepare("UPDATE `usuario` SET `numSesion` = :numSesion WHERE `id_usuario` = :id_usuario");
+        $stmt->bindParam(':numSesion', $numSesion, PDO::PARAM_INT);
+        $stmt->bindParam(':id_usuario', $id_usuario, PDO::PARAM_INT);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            // Obtener los valores actualizados
+            $resultadoActualizacion = array('id_usuario' => $id_usuario, 'numSesion' => $numSesion);
+            echo json_encode(array('isOk' => true, 'msj' => 'Registro editado de forma exitosa.', 'resultadoActualizacion' => $resultadoActualizacion));
+        } else {
+            echo json_encode(array('isOk' => false, 'msj' => 'No se encontraron registros para actualizar.'));
         }
-    } else {
-        http_response_code(400);
-        echo json_encode(array('isOk' => false, 'msj' => 'Faltan parámetros obligatorios.'));
     }
-} else {
-    http_response_code(405);
-    echo json_encode(array('isOk' => false, 'msj' => 'Método no permitido.'));
+} catch (PDOException $e) {
+    echo json_encode(array('isOk' => false, 'msj' => $e->getMessage()));
 }
 ?>
