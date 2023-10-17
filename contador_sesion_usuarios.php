0eba2c1c57e0429765b8c7555bf0c87991c2edcc
brodilli <brodilli_@hotmail.com>
2023-10-16 23:34:26 -0600

    Se moficó la api de contador sesion usuario antiguo

--- contador_sesion_usuarios.php
+++ contador_sesion_usuarios.php
@@ -14,6 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $id_usuario = $dataObject->id_usuario;
         $numSesion = $dataObject->numSesion;
 
+        // Realizar la actualización
         $actualizacion = "UPDATE `usuario` SET `numSesion` = :numSesion WHERE id_usuario = :id_usuario";
 
         $stmt = $conexion->prepare($actualizacion);
@@ -21,7 +22,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $stmt->bindParam(':numSesion', $numSesion);
 
         if ($stmt->execute()) {
-            echo json_encode(array('isOk' => true, 'msj' => 'Registro editado de forma exitosa.'));
+            // Consulta para obtener usuario_id y nombre
+            $consultaUsuario = "SELECT usuario_id, nombre FROM usuario WHERE id_usuario = :id_usuario";
+            $stmtConsulta = $conexion->prepare($consultaUsuario);
+            $stmtConsulta->bindParam(':id_usuario', $id_usuario);
+            $stmtConsulta->execute();
+            $usuario = $stmtConsulta->fetch(PDO::FETCH_ASSOC);
+
+            echo json_encode(array(
+                'isOk' => true,
+                'msj' => 'Registro editado de forma exitosa.',
+                'usuario_id' => $usuario['usuario_id'],
+                'nombre' => $usuario['nombre']
+            ));
         } else {
             echo json_encode(array('isOk' => false, 'msj' => 'Error al editar el registro: ' . $stmt->errorInfo()));
         }
