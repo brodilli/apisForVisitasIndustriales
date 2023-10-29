2031e65221e0d07b99d8975499b9b0a068f576d9
brodilli <brodilli_@hotmail.com>
2023-10-29 17:32:00 -0600

    cambio en la api de insertar registro prueba 3

--- insertar_empresa.php
+++ insertar_empresa.php
@@ -4,61 +4,51 @@ header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 header('Content-Type: application/json; charset=UTF-8');
 
 require 'conectar.php';
-
 $con = conectarDb();
 
-$data = json_decode(file_get_contents('php://input'));
+$data = json_decode(file_get_contents('php://input'), true); // Decodifica los datos JSON a un array asociativo
 
-$tipoUser = isset($data->tipoUser) ? $data->tipoUser : null;
-$nombres = isset($data->nombres) ? $data->nombres : null;
-$apellidoP = isset($data->apellidoP) ? $data->apellidoP : null;
-$apellidoM = isset($data->apellidoM) ? $data->apellidoM : null;
-$correo = isset($data->correo) ? $data->correo : null;
-$contraseña = isset($data->contraseña) ? $data->contraseña : null;
+// Extrae los valores del array asociativo
+$tipoUser = isset($data['tipoUser']) ? $data['tipoUser'] : null;
+$nombres = isset($data['nombres']) ? $data['nombres'] : null;
+$apellidoP = isset($data['apellidoP']) ? $data['apellidoP'] : null;
+$apellidoM = isset($data['apellidoM']) ? $data['apellidoM'] : null;
+$correo = isset($data['correo']) ? $data['correo'] : null;
+$contraseña = isset($data['contraseña']) ? $data['contraseña'] : null;
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Validar que los campos requeridos estén presentes en la solicitud y no estén vacíos
     if (
-        $tipoUser !== null &&
-        $nombres !== null &&
-        $apellidoP !== null &&
-        $apellidoM !== null &&
-        $correo !== null &&
-        $contraseña !== null
+        !empty($tipoUser) &&
+        !empty($nombres) &&
+        !empty($apellidoP) &&
+        !empty($apellidoM) &&
+        !empty($correo) &&
+        !empty($contraseña)
     ) {
-        try {
-            $stmt = $con->prepare("SELECT COUNT(*) FROM `usuario` WHERE `correo` = :correo");
-            $stmt->bindParam(':correo', $correo);
-            $stmt->execute();
-            $nums = $stmt->fetchColumn();
+        // Verificar si el correo ya está registrado
+        $result = mysqli_query($con, "SELECT * FROM `usuario` WHERE `correo` = '$correo'");
+        $nums = mysqli_num_rows($result);
 
-            if ($nums > 0) {
-                http_response_code(400); // Bad Request
-                echo json_encode(array('isOk' => 'existe', 'msj' => 'Correo ya registrado'));
+        if ($nums > 0) {
+            echo json_encode(array('isOk' => 'existe', 'msj' => 'Correo ya registrado'));
+        } else {
+            // Insertar el nuevo registro
+            $sqlQuery = "INSERT INTO `usuario` (`tipoUser`, `nombres`, `apellidoP`, `apellidoM`, `correo`, `contraseña`) 
+                VALUES ('$tipoUser', '$nombres', '$apellidoP', '$apellidoM', '$correo', '$contraseña')";
+
+            if ($con->query($sqlQuery) === TRUE) {
+                http_response_code(200);
+                echo json_encode(array('isOk' => 'true', 'msj' => 'Registro exitoso'));
             } else {
-                $stmt = $con->prepare("INSERT INTO `usuario` (`tipoUser`, `nombres`, `apellidoP`, `apellidoM`, `correo`, `contraseña`) 
-                    VALUES (:tipoUser, :nombres, :apellidoP, :apellidoM, :correo, :contraseña)");
-
-                $stmt->bindParam(':tipoUser', $tipoUser);
-                $stmt->bindParam(':nombres', $nombres);
-                $stmt->bindParam(':apellidoP', $apellidoP);
-                $stmt->bindParam(':apellidoM', $apellidoM);
-                $stmt->bindParam(':correo', $correo);
-                $stmt->bindParam(':contraseña', $contraseña);
-
-                if ($stmt->execute()) {
-                    http_response_code(200);
-                    echo json_encode(array('isOk' => 'true', 'msj' => 'Registro exitoso'));
-                } else {
-                    http_response_code(500); // Internal Server Error
-                    echo json_encode(array('isOk' => 'false', 'msj' => 'Error en la base de datos: ' . $stmt->errorInfo()));
-                }
+                http_response_code(500);
+                echo json_encode(array('isOk' => 'false', 'msj' => 'Error en la base de datos: ' . $con->error));
             }
-        } catch (PDOException $e) {
-            http_response_code(500);
-            echo json_encode(array('isOk' => 'false', 'msj' => 'Error en la base de datos: ' . $e->getMessage()));
         }
     } else {
         http_response_code(400); // Bad Request
         echo json_encode(array('isOk' => 'false', 'msj' => 'Faltan campos en la solicitud.'));
     }
+    mysqli_close($con);
 }
+?>
