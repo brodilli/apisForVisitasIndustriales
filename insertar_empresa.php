e5cbc2175ff1ab437d8450e026a873238a84fdf1
brodilli <brodilli_@hotmail.com>
2023-10-29 17:26:56 -0600

    cambio en la api de insertar registro prueba 2

--- insertar_empresa.php
+++ insertar_empresa.php
@@ -4,52 +4,61 @@ header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 header('Content-Type: application/json; charset=UTF-8');
 
 require 'conectar.php';
+
 $con = conectarDb();
 
 $data = json_decode(file_get_contents('php://input'));
 
-$nombre_empresa = isset($data->nombre_empresa) ? $data->nombre_empresa : null;
-$lugar = isset($data->lugar) ? $data->lugar : null;
-$nombre_contacto = isset($data->nombre_contacto) ? $data->nombre_contacto : null;
-$correo_contacto = isset($data->correo_contacto) ? $data->correo_contacto : null;
-$telefono_contacto = isset($data->telefono_contacto) ? $data->telefono_contacto : null;
+$tipoUser = isset($data->tipoUser) ? $data->tipoUser : null;
+$nombres = isset($data->nombres) ? $data->nombres : null;
+$apellidoP = isset($data->apellidoP) ? $data->apellidoP : null;
+$apellidoM = isset($data->apellidoM) ? $data->apellidoM : null;
+$correo = isset($data->correo) ? $data->correo : null;
+$contraseña = isset($data->contraseña) ? $data->contraseña : null;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (
-        $nombre_empresa !== null &&
-        $lugar !== null &&
-        $nombre_contacto !== null &&
-        $correo_contacto !== null &&
-        $telefono_contacto !== null
+        $tipoUser !== null &&
+        $nombres !== null &&
+        $apellidoP !== null &&
+        $apellidoM !== null &&
+        $correo !== null &&
+        $contraseña !== null
     ) {
         try {
-            $sqlQuery = "INSERT INTO `empresa`(`nombre_empresa`, `lugar`, `nombre_contacto`, `correo_contacto`, `telefono_contacto`)
-                VALUES (:nombre_empresa, :lugar, :nombre_contacto, :correo_contacto, :telefono_contacto)";
-            
-            $stmt = $con->prepare($sqlQuery);
-            $stmt->bindParam(':nombre_empresa', $nombre_empresa);
-            $stmt->bindParam(':lugar', $lugar);
-            $stmt->bindParam(':nombre_contacto', $nombre_contacto);
-            $stmt->bindParam(':correo_contacto', $correo_contacto);
-            $stmt->bindParam(':telefono_contacto', $telefono_contacto);
+            $stmt = $con->prepare("SELECT COUNT(*) FROM `usuario` WHERE `correo` = :correo");
+            $stmt->bindParam(':correo', $correo);
+            $stmt->execute();
+            $nums = $stmt->fetchColumn();
 
-            if ($stmt->execute()) {
-                http_response_code(200);
-                echo json_encode(array('isOk' => true, 'msj' => 'Registro exitoso'));
+            if ($nums > 0) {
+                http_response_code(400); // Bad Request
+                echo json_encode(array('isOk' => 'existe', 'msj' => 'Correo ya registrado'));
             } else {
-                http_response_code(500);
-                echo json_encode(array('isOk' => false, 'msj' => $con->error));
+                $stmt = $con->prepare("INSERT INTO `usuario` (`tipoUser`, `nombres`, `apellidoP`, `apellidoM`, `correo`, `contraseña`) 
+                    VALUES (:tipoUser, :nombres, :apellidoP, :apellidoM, :correo, :contraseña)");
+
+                $stmt->bindParam(':tipoUser', $tipoUser);
+                $stmt->bindParam(':nombres', $nombres);
+                $stmt->bindParam(':apellidoP', $apellidoP);
+                $stmt->bindParam(':apellidoM', $apellidoM);
+                $stmt->bindParam(':correo', $correo);
+                $stmt->bindParam(':contraseña', $contraseña);
+
+                if ($stmt->execute()) {
+                    http_response_code(200);
+                    echo json_encode(array('isOk' => 'true', 'msj' => 'Registro exitoso'));
+                } else {
+                    http_response_code(500); // Internal Server Error
+                    echo json_encode(array('isOk' => 'false', 'msj' => 'Error en la base de datos: ' . $stmt->errorInfo()));
+                }
             }
         } catch (PDOException $e) {
             http_response_code(500);
-            echo json_encode(array('isOk' => false, 'msj' => 'Error en la base de datos: ' . $e->getMessage()));
+            echo json_encode(array('isOk' => 'false', 'msj' => 'Error en la base de datos: ' . $e->getMessage()));
         }
     } else {
-        http_response_code(400);
-        echo json_encode(array('isOk' => false, 'msj' => 'Faltan parámetros obligatorios.'));
+        http_response_code(400); // Bad Request
+        echo json_encode(array('isOk' => 'false', 'msj' => 'Faltan campos en la solicitud.'));
     }
-} else {
-    http_response_code(405);
-    echo json_encode(array('isOk' => false, 'msj' => 'Método no permitido.'));
 }
-?>
