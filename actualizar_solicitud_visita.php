370d039cd635bbdc49dcb5fd9753a167155621d3
brodilli <brodilli_@hotmail.com>
2023-10-31 23:38:35 -0600

    prueba 6 de la api actualizar solicitud con reporte de errores vamos mejorando

--- actualizar_solicitud_visita.php
+++ actualizar_solicitud_visita.php
@@ -5,74 +5,58 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 require 'conectar.php';
+$con = conectarDb();
 
-$data = json_decode(file_get_contents("php://input"), true);
+$data = json_decode(file_get_contents("php://input"));
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $requiredFields = ['id_visita', 'id_carrera', 'id_empresa', 'semestre', 'grupo', 'objetivo', 'fecha', 'horaSalida', 'horaLlegada', 'num_alumnos', 'num_alumnas', 'asignatura', 'estatus', 'comentarios'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    try {
+        $requiredFields = ['id_visita', 'id_carrera', 'id_empresa', 'semestre', 'grupo', 'objetivo', 'fecha', 'horaSalida', 'horaLlegada', 'num_alumnos', 'num_alumnas', 'asignatura', 'estatus', 'comentarios'];
 
-    if (isset($data) && allRequiredFieldsExist($data, $requiredFields)) {
-        try {
-            $con = conectarDb();
-
-            $sql = "UPDATE solicitud_visita SET 
-                id_carrera = :id_carrera,
-                id_empresa = :id_empresa,
-                semestre = :semestre,
-                grupo = :grupo,
-                objetivo = :objetivo,
-                fecha = :fecha,
-                horaSalida = :horaSalida,
-                horaLlegada = :horaLlegada,
-                num_alumnos = :num_alumnos,
-                num_alumnas = :num_alumnas,
-                asignatura = :asignatura,
-                estatus = :estatus,
-                comentarios = :comentarios
-                WHERE id_visita = :id_visita";
-
-            $query = $con->prepare($sql);
-
-            $query->execute([
-                ':id_carrera' => $data['id_carrera'],
-                ':id_empresa' => $data['id_empresa'],
-                ':semestre' => $data['semestre'],
-                ':grupo' => $data['grupo'],
-                ':objetivo' => $data['objetivo'],
-                ':fecha' => $data['fecha'],
-                ':horaSalida' => $data['horaSalida'],
-                ':horaLlegada' => $data['horaLlegada'],
-                ':num_alumnos' => $data['num_alumnos'],
-                ':num_alumnas' => $data['num_alumnas'],
-                ':asignatura' => $data['asignatura'],
-                ':estatus' => $data['estatus'],
-                ':comentarios' => $data['comentarios'],
-                ':id_visita' => $data['id_visita']
-            ]);
-
-            if ($query->rowCount() > 0) {
-                http_response_code(200);
-                echo json_encode(['isOk' => true, 'msj' => 'Registro actualizado exitosamente']);
-            } else {
-                http_response_code(400);
-                echo json_encode(['isOk' => false, 'msj' => 'No se pudo actualizar el registro']);
+        foreach ($requiredFields as $field) {
+            if (!property_exists($data, $field)) {
+                throw new Exception("El campo '$field' es obligatorio.");
             }
-
-            $con = null; // Cerrar la conexión
-        } catch (PDOException $e) {
-            http_response_code(500);
-            echo json_encode(['isOk' => false, 'msj' => 'Error interno del servidor: ' . $e->getMessage()]);
         }
-    } else {
-        http_response_code(400);
-        echo json_encode(['isOk' => false, 'msj' => 'Datos insuficientes o inválidos']);
-    }
-}
 
-function allRequiredFieldsExist($data, $requiredFields) {
-    return count(array_intersect(array_keys($data), $requiredFields)) === count($requiredFields);
+        // Utiliza consultas preparadas para prevenir inyección SQL
+        $sqlQuery = $con->prepare("UPDATE solicitud_visita SET 
+            id_carrera = ?,
+            id_empresa = ?,
+            semestre = ?,
+            grupo = ?,
+            objetivo = ?,
+            fecha = ?,
+            horaSalida = ?,
+            horaLlegada = ?,
+            num_alumnos = ?,
+            num_alumnas = ?,
+            asignatura = ?,
+            estatus = ?,
+            comentarios = ?
+            WHERE id_visita = ?");
+
+        $sqlQuery->bind_param('iiissssssssssi', $data->id_carrera, $data->id_empresa, $data->semestre, $data->grupo, $data->objetivo, $data->fecha, $data->horaSalida, $data->horaLlegada, $data->num_alumnos, $data->num_alumnas, $data->asignatura, $data->estatus, $data->comentarios, $data->id_visita);
+        $sqlQuery->execute();
+
+        if ($sqlQuery->affected_rows > 0) {
+            http_response_code(200);
+            echo json_encode(['isOk' => true, 'msj' => 'Registro actualizado exitosamente']);
+        } else {
+            http_response_code(400);
+            echo json_encode(['isOk' => false, 'msj' => 'No se pudo actualizar el registro']);
+        }
+
+        $sqlQuery->close();
+        mysqli_close($con);
+    } catch (Exception $e) {
+        http_response_code(500);
+        echo json_encode(['isOk' => false, 'msj' => $e->getMessage()]);
+    }
+} else {
+    http_response_code(405);
+    echo json_encode(['isOk' => false, 'msj' => 'Método no permitido']);
 }
 ?>
