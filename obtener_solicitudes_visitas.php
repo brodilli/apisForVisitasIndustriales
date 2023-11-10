de6c89f411ee5e7eb242341ad76a4bb9914f46a6
brodilli <brodilli_@hotmail.com>
2023-11-09 22:57:56 -0600

    Prueba de obtener las solicitudes mediante el semestre esta si es la buena

--- obtener_solicitudes_visitas.php
+++ obtener_solicitudes_visitas.php
@@ -4,9 +4,12 @@ header("Content-Type: application/json; charset=utf-8");
 
 include "conectar.php"; // Asegúrate de incluir el archivo de conexión apropiado
 
-function obtenerSolicitudes() {
+function obtenerSolicitudes($rango) {
     try {
         $pdo = conectarDb(); // Conexión a la base de datos usando PDO
+
+        // Obtener el mes actual
+        $mes_actual = date('n');
 
         $sql = "SELECT solicitud_visita.id_visita, empresa.nombre_empresa, empresa.lugar, usuario.nombres, usuario.apellidoP, 
         usuario.apellidoM, usuario.id_usuario, solicitud_visita.fecha, solicitud_visita.horaSalida, solicitud_visita.horaLlegada, solicitud_visita.estatus, solicitud_visita.id_empresa, solicitud_visita.asignatura, solicitud_visita.objetivo, solicitud_visita.grupo, 
@@ -15,6 +18,15 @@ function obtenerSolicitudes() {
         INNER JOIN empresa ON solicitud_visita.id_empresa = empresa.id_empresa 
         INNER JOIN usuario ON solicitud_visita.id_usuario = usuario.id_usuario 
         INNER JOIN carrera ON solicitud_visita.id_carrera = carrera.id_carrera";
+
+        // Ajustar la consulta según el valor de la variable de rango
+        if ($rango == 1) {
+            if ($mes_actual >= 1 && $mes_actual <= 7) {
+                $sql .= " WHERE MONTH(solicitud_visita.fecha) >= 1 AND MONTH(solicitud_visita.fecha) <= 7";
+            } else {
+                $sql .= " WHERE MONTH(solicitud_visita.fecha) >= 8 AND MONTH(solicitud_visita.fecha) <= 12";
+            }
+        }
 
         $stmt = $pdo->prepare($sql);
         $stmt->execute();
@@ -31,8 +43,11 @@ function obtenerSolicitudes() {
     }
 }
 
+// Obtener el valor de la variable de rango, si no se proporciona, establecer en 2 por defecto
+$rango = isset($_GET['rango']) ? $_GET['rango'] : 2;
+
 // Llamada a la función para obtener los datos
-$solicitudes = obtenerSolicitudes();
+$solicitudes = obtenerSolicitudes($rango);
 
 // Devolver los datos como JSON
 echo json_encode($solicitudes);
