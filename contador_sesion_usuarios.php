20e278157497807c374a51d377d169adf3ed212c
brodilli <brodilli_@hotmail.com>
2023-11-12 23:50:49 -0600

    Se agregó el comentario de errores a las apis que hacían falta

--- contador_sesion_usuarios.php
+++ contador_sesion_usuarios.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 header('Access-Control-Allow-Methods: POST');
--- obtener_solicitudes_pdf.php
+++ obtener_solicitudes_pdf.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method');
 header('Content-Type: application/json; charset=utf-8');
