c5f3ac938d5be970dbd68f19b3fe51b04ff251c5
brodilli <brodilli_@hotmail.com>
2023-12-03 23:44:15 -0600

    se agregó una correccion para no repetir agenda

--- no_repetir_agenda.php
+++ no_repetir_agenda.php
@@ -3,58 +3,43 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Content-Type: application/json; charset=utf-8");
-
+header("Content-Type: text/html; charset=utf-8");
 $method = $_SERVER['REQUEST_METHOD'];
 
 $data = json_decode(file_get_contents("php://input"));
-$id_visita = $data->id_visita;
+$id_visita = $data -> id_visita;
 
-// Evitar la inyección de SQL usando consultas preparadas
-$sql = "SELECT COUNT(*) AS count FROM `agenda` WHERE id_visita = ?";
+
+$sql= "SELECT COUNT(*) AS count FROM `agenda` WHERE id_visita = '$id_visita'";
 include "conectar.php";
 
-function desconectar($conexion)
-{
+//sleep(1);
+function desconectar($conexion){
+
     $close = mysqli_close($conexion);
 
-    if ($close) {
-        echo '';
-    } else {
-        echo 'Ha sucedido un error inexperado en la conexión de la base de datos';
-    }
+        if($close){
+            echo '';
+        }else{
+            echo 'Ha sucedido un error inexperado en la conexión de la base de datos';
+        }
 
     return $close;
 }
 
-function obtenerArreglo($sql, $id_visita)
-{
+function obtenerArreglo($sql){
     $conexion = conectarDB();
+
     mysqli_set_charset($conexion, "utf8");
 
-    // Utilizar consultas preparadas para evitar la inyección de SQL
-    $stmt = mysqli_prepare($conexion, $sql);
+    if(!$resultado = mysqli_query($conexion, $sql)) die();
 
-    if (!$stmt) {
-        die('Error en la preparación de la consulta: ' . mysqli_error($conexion));
-    }
-
-    // Vincular parámetros
-    mysqli_stmt_bind_param($stmt, "s", $id_visita);
-
-    // Ejecutar la consulta
-    mysqli_stmt_execute($stmt);
-
-    // Obtener el resultado
-    $resultado = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($resultado);
-
-    // Desconectar la base de datos
     desconectar($conexion);
 
     return $row;
 }
 
-$r = obtenerArreglo($sql, $id_visita);
+$r = obtenerArreglo($sql);
 echo json_encode($r);
 ?>
